6443aef38308fcd841352824c5c00e49c80679ec
tecnov <davide_desimone@me.com>
2019-03-26 16:11:01 +0100

    first commit

---
+++ src/it/homebank/sportello/model/Cliente.java
@@ -0,0 +1,68 @@
+package model;
+
+public class Cliente {
+
+    private String nome;
+    private String cognome;
+    private String email;
+    private String username;
+    private String password;
+    private ContoCorrente contoCorrente;
+
+    public Cliente(String nome, String cognome, String email, String username, String password, ContoCorrente contoCorrente) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.contoCorrente = contoCorrente;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public ContoCorrente getContoCorrente() {
+        return contoCorrente;
+    }
+
+    public void setContoCorrente(ContoCorrente contoCorrente) {
+        this.contoCorrente = contoCorrente;
+    }
+}
---
+++ src/it/homebank/sportello/model/Direttore.java
@@ -0,0 +1,11 @@
+package it.homebank.sportello.model;
+
+public class Direttore {
+
+    private String nome;
+    private String cognome;
+    private String email;
+    private String username;
+    private String password;
+    private String descrizione;
+}
