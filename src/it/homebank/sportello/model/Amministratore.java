cf0218084bc87f70b2c8dba12d778a089271c39e
tecnov <davide_desimone@me.com>
2019-03-28 15:40:11 +0100

    cose nuove a caso1

--- src/it/homebank/sportello/model/Amministratore.java
+++ src/it/homebank/sportello/model/Amministratore.java
@@ -1,8 +1,7 @@
 package it.homebank.sportello.model;
 
-public class Amministratore extends Utente {
+public class Amministratore {
+
+    private int idAmministratore;
     int
-    public Amministratore(String nome, String cognome, String email, String username, String password) {
-        super(nome, cognome, email, username, password);
-    }
 }
