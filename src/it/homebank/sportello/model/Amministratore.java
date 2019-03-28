7f13a4361560cb92127d15142d079b192984e910
Andrea <andre.degiorgi8007@gmail.com>
2019-03-28 15:45:19 +0100

    Revert "push senza persona"
    This reverts commit 50b0ad01

--- src/it/homebank/sportello/model/Amministratore.java
+++ src/it/homebank/sportello/model/Amministratore.java
@@ -1,7 +1,31 @@
 package it.homebank.sportello.model;
 
 public class Amministratore {
-
     private int idAmministratore;
-    int
+    private String password;
+
+    public Amministratore() {
+
+    }
+
+    public Amministratore(int idAmministratore) {
+        this.idAmministratore = idAmministratore;
+    }
+
+
+    public int getIdAmministratore() {
+        return idAmministratore;
+    }
+
+    public void setIdAmministratore(int idAmministratore) {
+        this.idAmministratore = idAmministratore;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
