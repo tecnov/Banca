db39577b596a3b6fcf74ab138ba12e489ad22352
tecnov <davide_desimone@me.com>
2019-03-29 16:24:09 +0100

    cose nuove a caso1

--- src/it/homebank/sportello/model/Amministratore.java
+++ src/it/homebank/sportello/model/Amministratore.java
@@ -1,31 +1,7 @@
 package it.homebank.sportello.model;
 
 public class Amministratore {
+
     private int idAmministratore;
-    private String password;
-
-    public Amministratore() {
-
-    }
-
-    public Amministratore(int idAmministratore) {
-        this.idAmministratore = idAmministratore;
-    }
-
-
-    public int getIdAmministratore() {
-        return idAmministratore;
-    }
-
-    public void setIdAmministratore(int idAmministratore) {
-        this.idAmministratore = idAmministratore;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    int
 }
